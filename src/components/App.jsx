433fd731e315aa770e5dbff0b75cd8494cb7d206
vladvl1 <114438226+vladvl1@users.noreply.github.com>
2023-06-03 11:04:29 +0300

    5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
+import React, {Component} from "react"
+import Statistics from './Statistics/Statistics'
 
 
-
-
-export const App = () => {
-  return (
-    <div>
-      <h1>Please leave feedback</h1>
-      
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+  render() {
+    return (
+      <>
+      <div>
+        <h1>Please leave feedback</h1>
+      <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} />
+        </div>
+        </>
+    );
+  }
+}
+export default App;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,2 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
+
+
+
+class Statistics extends Component {
+
+}
+export default Statistics;
